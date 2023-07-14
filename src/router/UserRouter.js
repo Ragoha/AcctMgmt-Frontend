40e77e5ded24e1b9ca3bb74923dacf425498cdda
pshongi <84509855+pshongi@users.noreply.github.com>
2023-07-14 17:41:54 +0900

    feat. 예산과목등록_3
    3333

--- src/router/UserRouter.js
+++ src/router/UserRouter.js
@@ -1,18 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import TimeUserComponent from "../component/user/TimeUserComponent";
 import ListUserComponent from "../component/user/ListUserComponent";
-import { Component } from "react";
+import BoardDataComponent from "../component/user/BoardDataComponent";
+import Budget_item_registration from "../component/Budget_item_registration/Budget_item_registration";
 
-
-class UserRouter extends Component{
-  render(){
-    return (
-      <Routes>
-        <Route path="/user/time" element={<TimeUserComponent />} />
-        <Route path="/user/list" element={<ListUserComponent />} />
-      </Routes>
-    );
-  }
+export default function UserRouter() {
+  return (
+    <Routes>
+      <Route path="/time" element={<TimeUserComponent />} />
+      <Route path="/list" element={<ListUserComponent />} />
+      <Route path="/boardData" element={<BoardDataComponent/>}/>
+      <Route path="/Budget_item_registration" element={<Budget_item_registration/>}/>
+    </Routes>
+  );
 }
-
-export default UserRouter;
