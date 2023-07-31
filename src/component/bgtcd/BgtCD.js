72292cdfb24492c1b26787e115038638198c7f16
pshongi <84509855+pshongi@users.noreply.github.com>
2023-07-31 13:30:50 +0900

    fix 230731 front 작업 시작전 커밋
    fix 230731 front 작업 시작전 커밋

--- src/component/bgtcd/BgtCD.js
+++ src/component/bgtcd/BgtCD.js
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Grid, InputLabel, TextField } from "@mui/material";
+import { Autocomplete, Box, Button, Container, Grid, InputLabel, TextField } from "@mui/material";
 import React, { Component } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import BgtCDService from "../../service/BgtCDService";
@@ -6,6 +6,7 @@ import BgtCDDevFgCustom from "./modal/BgtCDDevFgCustom";
 import BgtCDAddSubDialog from "./modal/BgtCDAddSubDialog";
 import BgtCDDetailInfo from "./BgtCDDetailInfo";
 import BgtCDDatagrid from "./BgtCDDatagrid";
+import BgtCDGroupModal from "./modal/BgtCDGroupModal";
 
 class BgtCD extends Component {
     constructor(props) {
@@ -14,18 +15,20 @@ class BgtCD extends Component {
         this.BgtCDAddSubDialog = React.createRef();
         this.BgtDataGrid = React.createRef();
         this.BgtCDDetailInfo = React.createRef();
+        this.BgtCDGroupModal = React.createRef();
         this.state = {
             open: false,
             rows: [],
             groupcd: 'GROUP3',
             focus: null,
+            testList: ['블레', '블래', '리퍼', '소서', '건슬', '기공', '알카'],
         }
     }
     /*상단 조건 검색바 start*/
 
     /*상단 조건 검색바 end  */
     setDetailInfo = (target) => {
-        console.log('패런트')
+        console.log('---setDetailInfo---')
         console.log(target)
         this.BgtCDDetailInfo.current.setDetailInfo(target);
     }
@@ -41,32 +44,25 @@ class BgtCD extends Component {
                 console.error("Error fetching data:", error);
             });
     }
-    setDetailInfoFromClickedRow(detailInfo, param) {//clickedRow로 데이터그리드를 클릭했을 때 받아온 array를 detailInfo에 매핑하는 메소드 
-        //[SBGTCDDomain(CO_CD=null, BGT_CD=null, GISU=0, BGT_NM=null, DIV_FG=null, ㄱCTL_FG=3, ㄴBGAJUST_FG=1, ㄷTO_DT=20231231, ㄹBOTTOM_FG=0, ㅁBIG_FG=null)]
-        console.log('4.BGTCD의 setDetailInfoFromClickedRow 실행 해볼까 ~~~!');
-        this.setState({
-            ctlFg: detailInfo[0].ctlFg,
-            bgajustFg: detailInfo[0].bgajustFg,
-            bottomFg: detailInfo[0].bottomFg,
-            bizFg: detailInfo[0].bizFg,
-        });
-    }
     //데이터 그리드에 추가하는 기능
+    //[230728] TreeView 수정했는데 addRow 로직은 아직 변경하지 않음 한번 손봐야함
     handleRowAdd = () => {
         //DetailInfo 에 저장된 BgtCd 값을 가져와 ->
         console.log('추가버튼')
         console.log(this.BgtCDDetailInfo.current.getBgtCd())
         const bgtCd = this.BgtCDDetailInfo.current.getBgtCd();
         BgtCDService.getPath(bgtCd)
-        .then((path)=>{ 
-            console.log("패스는 ? : " + path) //이 패스는 현재 찍은 위치의 path이다 
-        }) 
+            .then((path) => {
+                console.log("패스는 ? : " + path) //이 패스는 현재 찍은 위치의 path이다 
+                path = "수입,장,관,항                                  ";
+                const newRows = [
+                    ...this.state.rows,
+                    { dataPath: path, defNm: "", bgtCd: "", bgtNm: "", isNew: true },
+                ];
+                this.setState({ rows: newRows });
+            })
         // *일단 로우를 특정하기부터 ..
-        const newRows = [
-            ...this.state.rows,
-            { defNm: "", bgtCd: "", bgtNm: "", isNew: true },
-        ];
-        this.setState({ rows: newRows });
+
     };
 
     /*  데이터 그리드 부분 end */
@@ -79,19 +75,31 @@ class BgtCD extends Component {
     BgtCDAddSubDialogOpen = () => {
         this.BgtCDAddSubDialog.current.handleUp();
     }
+    BgtCDGroupModalOpen = ()=>{
+        this.BgtCDGroupModal.current.handleUp();
+    }
     render() {
         const { rows, groupcd, ctlFg, bgajustFg, bottomFg, bizFg, prevBgtCd } = this.state;
         return (
             <>
-                <Grid container>
+                <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        <Box
-                            sx={{ backgroundColor: '#7895CB', height: '50px', alignItems: 'center', display: 'flex', border: '1px solid' }}
-                            padding={1}>
+                        <Box 
+                            sx={{ 
+                                backgroundColor: '#7895CB',
+                                height: '50px', 
+                                alignItems: 'center', 
+                                display: 'flex', 
+                                border: '1px solid' }}
+                            padding={2}>
                             <Button variant="contained"
                                 size="medium"
                                 onClick={this.handleRowAdd}
-                                style={{ marginLeft: 'auto', marginRight: '10px', border: '1px solid' }}
+                                style={{ 
+                                marginLeft: 'auto', 
+                                marginRight: '10px', 
+                                border: '1px solid' 
+                                }}
                             >추가</Button>
                             <Button variant="contained"
                                 size="medium"
@@ -104,16 +112,34 @@ class BgtCD extends Component {
                             <Button variant="contained" size="medium" style={{ marginRight: '10px', border: '1px solid' }} onClick={this.BgtCDDevFgCustomOpen}>
                                 그룹레벨설정
                             </Button>
-                            <Button variant="contained" size="medium" style={{ border: '1px solid' }} onClick={this.Pold} >기능모음</Button>
+                            <Button variant="contained" size="medium" style={{ border: '1px solid' }} >기능모음</Button>
                         </Box>
                     </Grid>
                     <Grid item xs={12}>
                         <Box
                             sx={{ height: '40px', alignItems: 'center', display: 'flex', border: '1px solid' }}
-                            padding={1}
+                            padding={4}
                             variant>
-                            <InputLabel sx={{ marginLeft: '20px' }}>예산그룹</InputLabel><TextField onChange={this.GroupCdOnChange} size="small" inputProps={{ style: { height: '11px' } }} sx={{ width: '200px', marginRight: '50px' }} />
-                            <InputLabel>예산과목코드</InputLabel><TextField onChange={this.Bgt_nmOnChange} size="small" inputProps={{ style: { height: '11px' } }} sx={{ width: '200px', marginRight: '50px' }} />
+                            {/* <InputLabel sx={{ marginLeft: '20px' }}>예산그룹</InputLabel><TextField onChange={this.GroupCdOnChange} size="small" inputProps={{ style: { height: '11px' } }} sx={{ width: '200px', marginRight: '50px' }} /> */}
+                            <InputLabel>테스트용 리스트</InputLabel>
+                            <Autocomplete
+                                //  autoComplete
+                               openOnFocus
+                               // clearOnEscape
+                                options={[
+                                    { label: "리퍼", value: "deran" },
+                                    { label: "블레", value: "deran" },
+                                    { label: "데모닉", value: "deran" },
+                                    { label: "소울", value: "deran" },
+                                    { label: "The GodFather", value: "deran" },
+                                ]}
+                                variant="standard"
+                                size="small"
+                                sx={{ width: '200px', marginRight: '50px', }}
+                                
+                                renderInput={(params) => <TextField {...params}/>}
+                            ></Autocomplete>
+                            <InputLabel>예산과목코드</InputLabel><TextField onClick={this.BgtCDGroupModalOpen} onChange={this.Bgt_nmOnChange} size="small" inputProps={{ style: { height: '11px' } }} sx={{ width: '200px', marginRight: '50px' }} />
                             <InputLabel>예산과목명</InputLabel><TextField onChange={this.Bgt_cdOnChange} size="small" inputProps={{ style: { height: '11px' } }} sx={{ width: '200px' }} />
                             <Button onClick={this.searchClick}><SearchIcon /></Button>
                         </Box>
@@ -127,6 +153,7 @@ class BgtCD extends Component {
                 </Grid>
                 <BgtCDDevFgCustom ref={this.BgtCDDevFgCustom} />
                 <BgtCDAddSubDialog ref={this.BgtCDAddSubDialog} />
+                <BgtCDGroupModal ref={this.BgtCDGroupModal}/>
             </>
         )
     }
--- src/service/BgtCDService.js
+++ src/service/BgtCDService.js
@@ -30,7 +30,7 @@ class BgtCDService {
 
         return returnData1;
     }
-    getBgtCDTerm(CO_CD){ //(★임시로 데이터 테스트 용으로 만든것, 반드시 수정해야함!!!) BgtCD 그룹레벨설정 할때 초기값.
+    getBgtCDTerm(CO_CD){ //(★임시로 데이터 테스트 용 CoCd, 반드시 수정해야함!!!) BgtCD 그룹레벨설정 할때 초기값.
        
         console.log("찍어보자 : " + CO_CD)
         const returnData=axios.get(ACCTMGMT_API_BASE_URL + "/getBgtCDTerm" ,{
